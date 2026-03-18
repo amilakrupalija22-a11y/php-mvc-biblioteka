a8a240b8d65fe10366c6b2c949eb15b34f35ecbb
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-18 12:42:49 +0100

    Update Book.php

--- mvc-projekat/models/Book.php
+++ mvc-projekat/models/Book.php
@@ -19,4 +19,16 @@ class Book {
         global $conn;
         return $conn->query("DELETE FROM books WHERE id=$id");
     }
+
+    public static function update($id, $title, $author, $category_id) {
+    global $conn;
+    $sql = "UPDATE books SET title='$title', author='$author', category_id='$category_id' WHERE id=$id";
+    return $conn->query($sql);
+}
+
+    public static function find($id) {
+    global $conn;
+    $result = $conn->query("SELECT * FROM books WHERE id=$id");
+    return $result->fetch_assoc();
+}
 }
