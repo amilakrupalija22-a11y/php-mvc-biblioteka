c1dc1389953bb017da563518d6af7dbaa8b9309e
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-18 13:02:48 +0100

    Replace category ID input with dropdown selection

--- mvc-projekat/views/books/create.php
+++ mvc-projekat/views/books/create.php
@@ -1,10 +1,22 @@
+<?php
+require_once "../../config/database.php"; 
+
+
+$categories = $conn->query("SELECT * FROM categories");
+?>
+
 <form method="POST" action="index.php?action=store" class="container mt-5">
 
-<input type="text" name="title" placeholder="Naslov" class="form-control mb-2" required>
-<input type="text" name="author" placeholder="Autor" class="form-control mb-2" required>
+    <input type="text" name="title" placeholder="Naslov" class="form-control mb-2" required>
+    <input type="text" name="author" placeholder="Autor" class="form-control mb-2" required>
 
-<input type="number" name="category_id" placeholder="Kategorija ID" class="form-control mb-2">
+    <select name="category_id" class="form-control mb-2" required>
+        <option value="">Odaberi kategoriju</option>
+        <?php while($cat = $categories->fetch_assoc()): ?>
+            <option value="<?= $cat['id'] ?>"><?= $cat['name'] ?></option>
+        <?php endwhile; ?>
+    </select>
 
-<button class="btn btn-primary">Sačuvaj</button>
+    <button class="btn btn-primary">Sačuvaj</button>
 
 </form>
