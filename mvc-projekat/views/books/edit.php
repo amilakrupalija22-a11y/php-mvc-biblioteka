9c108cb043d6545d0fcdde27243ceaee8355f88e
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-18 13:15:25 +0100

    Update edit.php

--- mvc-projekat/views/books/edit.php
+++ mvc-projekat/views/books/edit.php
@@ -1,17 +1,18 @@
-<?php
-
-?>
 <form method="POST" action="index.php?action=update" class="container mt-5">
+
     <input type="hidden" name="id" value="<?= $book['id'] ?>">
 
-    <input type="text" name="title" placeholder="Naslov" class="form-control mb-2" 
-           value="<?= $book['title'] ?>" required>
+    <input type="text" name="title" placeholder="Naslov" class="form-control mb-2" value="<?= $book['title'] ?>" required>
+    <input type="text" name="author" placeholder="Autor" class="form-control mb-2" value="<?= $book['author'] ?>" required>
 
-    <input type="text" name="author" placeholder="Autor" class="form-control mb-2" 
-           value="<?= $book['author'] ?>" required>
+    <select name="category_id" class="form-control mb-2" required>
+        <?php foreach($categories as $cat): ?>
+            <option value="<?= $cat['id'] ?>" <?= $cat['id']==$book['category_id']?'selected':'' ?>>
+                <?= $cat['name'] ?>
+            </option>
+        <?php endforeach; ?>
+    </select>
 
-    <input type="number" name="category_id" placeholder="Kategorija ID" class="form-control mb-2" 
-           value="<?= $book['category_id'] ?>">
+    <button class="btn btn-primary">Sačuvaj promjene</button>
 
-    <button class="btn btn-primary">Sačuvaj izmjene</button>
 </form>
