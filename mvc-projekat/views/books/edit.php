1d8204e29aed1c2049d0484094b3c9a24655ead1
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-18 12:46:47 +0100

    Add form for editing book details

--- mvc-projekat/views/books/edit.php
+++ mvc-projekat/views/books/edit.php
@@ -1,1 +1,17 @@
+<?php
 
+?>
+<form method="POST" action="index.php?action=update" class="container mt-5">
+    <input type="hidden" name="id" value="<?= $book['id'] ?>">
+
+    <input type="text" name="title" placeholder="Naslov" class="form-control mb-2" 
+           value="<?= $book['title'] ?>" required>
+
+    <input type="text" name="author" placeholder="Autor" class="form-control mb-2" 
+           value="<?= $book['author'] ?>" required>
+
+    <input type="number" name="category_id" placeholder="Kategorija ID" class="form-control mb-2" 
+           value="<?= $book['category_id'] ?>">
+
+    <button class="btn btn-primary">Sačuvaj izmjene</button>
+</form>
