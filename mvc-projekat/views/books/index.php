9017267d886b8efa7f2ecaa0d6ae7e621c184b0a
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-18 13:14:30 +0100

    Update index.php

--- mvc-projekat/views/books/index.php
+++ mvc-projekat/views/books/index.php
@@ -3,18 +3,17 @@
 <head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-
 <body class="container mt-5">
 
 <h2>Knjige</h2>
+<a href="index.php?action=create" class="btn btn-success mb-3">Dodaj knjigu</a>
 
-<a href="index.php?action=create" class="btn btn-success">Dodaj knjigu</a>
-
-<table class="table mt-3">
+<table class="table table-bordered">
 <tr>
     <th>ID</th>
     <th>Naslov</th>
     <th>Autor</th>
+    <th>Kategorija</th>
     <th>Akcije</th>
 </tr>
 
@@ -23,15 +22,14 @@
     <td><?= $row['id'] ?></td>
     <td><?= $row['title'] ?></td>
     <td><?= $row['author'] ?></td>
+    <td><?= $row['category_name'] ?></td>
     <td>
-        <a href="index.php?action=delete&id=<?= $row['id'] ?>"
-           onclick="return confirm('Da li si sigurna?')"
-           class="btn btn-danger">Obriši</a>
+        <a href="index.php?action=edit&id=<?= $row['id'] ?>" class="btn btn-warning">Uredi</a>
+        <a href="index.php?action=delete&id=<?= $row['id'] ?>" onclick="return confirm('Da li si sigurna?')" class="btn btn-danger">Obriši</a>
     </td>
 </tr>
 <?php endwhile; ?>
 
 </table>
-
 </body>
 </html>
