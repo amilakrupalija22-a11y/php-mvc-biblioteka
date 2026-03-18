81dce17f5a0d753fe82c1f2f59b759c0e03362bc
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-18 12:34:54 +0100

    Add form for creating new book entries

--- mvc-projekat/views/books/create.php
+++ mvc-projekat/views/books/create.php
@@ -1,1 +1,10 @@
+<form method="POST" action="index.php?action=store" class="container mt-5">
 
+<input type="text" name="title" placeholder="Naslov" class="form-control mb-2" required>
+<input type="text" name="author" placeholder="Autor" class="form-control mb-2" required>
+
+<input type="number" name="category_id" placeholder="Kategorija ID" class="form-control mb-2">
+
+<button class="btn btn-primary">Sačuvaj</button>
+
+</form>
