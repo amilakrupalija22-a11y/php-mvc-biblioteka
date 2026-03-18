5457980bba5e0fb7b8804ff5d7e02bb0cde274bf
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-18 12:34:30 +0100

    Update index.php

--- mvc-projekat/views/books/index.php
+++ mvc-projekat/views/books/index.php
@@ -1,1 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
 
+<body class="container mt-5">
+
+<h2>Knjige</h2>
+
+<a href="index.php?action=create" class="btn btn-success">Dodaj knjigu</a>
+
+<table class="table mt-3">
+<tr>
+    <th>ID</th>
+    <th>Naslov</th>
+    <th>Autor</th>
+    <th>Akcije</th>
+</tr>
+
+<?php while($row = $books->fetch_assoc()): ?>
+<tr>
+    <td><?= $row['id'] ?></td>
+    <td><?= $row['title'] ?></td>
+    <td><?= $row['author'] ?></td>
+    <td>
+        <a href="index.php?action=delete&id=<?= $row['id'] ?>"
+           onclick="return confirm('Da li si sigurna?')"
+           class="btn btn-danger">Obriši</a>
+    </td>
+</tr>
+<?php endwhile; ?>
+
+</table>
+
+</body>
+</html>
