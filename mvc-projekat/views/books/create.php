05dc097407b450b5acce9f2c1e5c7ab7f9b78ebd
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-18 13:06:04 +0100

    Refactor category selection to use foreach loop

--- mvc-projekat/views/books/create.php
+++ mvc-projekat/views/books/create.php
@@ -1,8 +1,5 @@
 <?php
-require_once "../../config/database.php"; 
 
-
-$categories = $conn->query("SELECT * FROM categories");
 ?>
 
 <form method="POST" action="index.php?action=store" class="container mt-5">
@@ -12,9 +9,9 @@ $categories = $conn->query("SELECT * FROM categories");
 
     <select name="category_id" class="form-control mb-2" required>
         <option value="">Odaberi kategoriju</option>
-        <?php while($cat = $categories->fetch_assoc()): ?>
+        <?php foreach($categories as $cat): ?>
             <option value="<?= $cat['id'] ?>"><?= $cat['name'] ?></option>
-        <?php endwhile; ?>
+        <?php endforeach; ?>
     </select>
 
     <button class="btn btn-primary">Sačuvaj</button>
