78ff0c657c938e4f72b13b0ee5de84b3a4267457
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-29 13:09:18 +0200

    Implement CategoryController with index method

--- mvc-projekat/controllers/CategoryController.php
+++ mvc-projekat/controllers/CategoryController.php
@@ -1,1 +1,15 @@
+<?php
+require_once "../models/Category.php";
 
+class CategoryController {
+    private $category;
+
+    public function __construct($db) {
+        $this->category = new Category($db);
+    }
+
+    public function index() {
+        $categories = $this->category->getAll();
+        include "../views/categories/index.php";
+    }
+}
