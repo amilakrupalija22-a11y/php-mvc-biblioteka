21f009093ee3cd1d131612e6a38c62e28f8f878e
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-29 13:02:45 +0200

    Refactor edit form for book details

--- mvc-projekat/views/books/edit.php
+++ mvc-projekat/views/books/edit.php
@@ -1,18 +1,13 @@
-<form method="POST" action="index.php?action=update" class="container mt-5">
+<?php include "../views/layout/header.php"; ?>
 
+<form method="POST">
     <input type="hidden" name="id" value="<?= $book['id'] ?>">
 
-    <input type="text" name="title" placeholder="Naslov" class="form-control mb-2" value="<?= $book['title'] ?>" required>
-    <input type="text" name="author" placeholder="Autor" class="form-control mb-2" value="<?= $book['author'] ?>" required>
+    <input type="text" name="title" value="<?= $book['title'] ?>" class="form-control mb-2">
+    <input type="text" name="author" value="<?= $book['author'] ?>" class="form-control mb-2">
+    <input type="number" name="category_id" value="<?= $book['category_id'] ?>" class="form-control mb-2">
 
-    <select name="category_id" class="form-control mb-2" required>
-        <?php foreach($categories as $cat): ?>
-            <option value="<?= $cat['id'] ?>" <?= $cat['id']==$book['category_id']?'selected':'' ?>>
-                <?= $cat['name'] ?>
-            </option>
-        <?php endforeach; ?>
-    </select>
-
-    <button class="btn btn-primary">Sačuvaj promjene</button>
-
+    <button class="btn btn-success">Update</button>
 </form>
+
+<?php include "../views/layout/footer.php"; ?>
