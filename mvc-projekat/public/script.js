b165b1858fcfdea8d2e00dc282ed3b4e474a8e84
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-29 13:13:07 +0200

    Update script.js

--- mvc-projekat/public/script.js
+++ mvc-projekat/public/script.js
@@ -1,8 +1,21 @@
 
 document.querySelectorAll('.delete-btn').forEach(btn => {
     btn.addEventListener('click', function(e) {
-        if (!confirm("Da li ste sigurni?")) {
+        if (!confirm("Da li ste sigurni da želite obrisati?")) {
             e.preventDefault();
         }
     });
 });
+
+
+
+let search = document.getElementById("search");
+if (search) {
+    search.addEventListener("keyup", function() {
+        let value = this.value.toLowerCase();
+        document.querySelectorAll("tbody tr").forEach(row => {
+            row.style.display =
+                row.innerText.toLowerCase().includes(value) ? "" : "none";
+        });
+    });
+}
