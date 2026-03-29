ff777679c10e231d7223e6c9ea3d6b2fc8ea0e2a
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-29 13:08:03 +0200

    Update database.php

--- mvc-projekat/config/database.php
+++ mvc-projekat/config/database.php
@@ -6,11 +6,16 @@ class Database {
     private $password = "";
 
     public function connect() {
-        $conn = new PDO(
-            "mysql:host=$this->host;dbname=$this->db_name",
-            $this->username,
-            $this->password
-        );
-        return $conn;
+        try {
+            $conn = new PDO(
+                "mysql:host=$this->host;dbname=$this->db_name;charset=utf8",
+                $this->username,
+                $this->password
+            );
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch (PDOException $e) {
+            echo "Greška konekcije: " . $e->getMessage();
+        }
     }
 }
