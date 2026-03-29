4849cb3d4dd608bdb5590233e3a298a1ec0a585d
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-29 12:45:50 +0200

    Refactor database connection to use PDO

--- mvc-projekat/config/database.php
+++ mvc-projekat/config/database.php
@@ -1,7 +1,16 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "biblioteka");
+class Database {
+    private $host = "localhost";
+    private $db_name = "biblioteka";
+    private $username = "root";
+    private $password = "";
 
-if ($conn->connect_error) {
-    die("Greška: " . $conn->connect_error);
+    public function connect() {
+        $conn = new PDO(
+            "mysql:host=$this->host;dbname=$this->db_name",
+            $this->username,
+            $this->password
+        );
+        return $conn;
+    }
 }
-?>
