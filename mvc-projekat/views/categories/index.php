3999d07e38fd30823d128aea714eab85bba33978
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-29 13:12:41 +0200

    Display categories in a table format

--- mvc-projekat/views/categories/index.php
+++ mvc-projekat/views/categories/index.php
@@ -1,1 +1,20 @@
+<?php include "../views/layout/header.php"; ?>
 
+<h3>Kategorije</h3>
+
+<table class="table table-bordered">
+<tr>
+    <th>ID</th>
+    <th>Naziv</th>
+</tr>
+
+<?php foreach($categories as $c): ?>
+<tr>
+    <td><?= $c['id'] ?></td>
+    <td><?= $c['name'] ?></td>
+</tr>
+<?php endforeach; ?>
+
+</table>
+
+<?php include "../views/layout/footer.php"; ?>
