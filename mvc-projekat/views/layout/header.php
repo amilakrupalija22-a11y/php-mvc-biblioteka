c95ab57960b3853d94203b25a4f004df0650cb3b
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-29 13:10:40 +0200

    Update header.php

--- mvc-projekat/views/layout/header.php
+++ mvc-projekat/views/layout/header.php
@@ -5,6 +5,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+
+<nav class="navbar navbar-dark bg-dark px-3">
+    <a class="navbar-brand" href="index.php">📚 Biblioteka</a>
+    <div>
+        <a href="index.php" class="btn btn-light btn-sm">Knjige</a>
+        <a href="index.php?controller=category" class="btn btn-light btn-sm">Kategorije</a>
+    </div>
+</nav>
+
 <div class="container mt-4">
-<h2 class="text-center">📚 Biblioteka knjiga</h2>
-<hr>
