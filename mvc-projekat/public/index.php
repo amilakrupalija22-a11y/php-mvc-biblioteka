91fcdbc8370da28e2bf9c894114865590c217956
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-29 13:00:25 +0200

    Refactor index.php for database integration
    Refactor index.php to include database connection and streamline action handling.

--- mvc-projekat/public/index.php
+++ mvc-projekat/public/index.php
@@ -1,26 +1,22 @@
 <?php
+require_once "../config/database.php";
 require_once "../controllers/BookController.php";
 
-$controller = new BookController();
+$db = (new Database())->connect();
+$controller = new BookController($db);
+
 $action = $_GET['action'] ?? 'index';
 
-switch($action) {
+switch ($action) {
     case 'create':
         $controller->create();
-        break;
-    case 'store':
-        $controller->store();
-        break;
-    case 'edit':
-        $controller->edit();
-        break;
-    case 'update':
-        $controller->update();
         break;
     case 'delete':
         $controller->delete();
         break;
+    case 'edit':
+        $controller->edit();
+        break;
     default:
         $controller->index();
 }
-?>
