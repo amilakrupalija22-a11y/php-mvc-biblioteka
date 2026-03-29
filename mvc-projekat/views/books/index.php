857928520121dd0eb969a7936b7241ea0444def8
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-29 13:11:31 +0200

    Refactor book index page layout and add search input

--- mvc-projekat/views/books/index.php
+++ mvc-projekat/views/books/index.php
@@ -1,27 +1,34 @@
 <?php include "../views/layout/header.php"; ?>
 
-<a href="index.php?action=create" class="btn btn-primary mb-3">Dodaj knjigu</a>
+<h3>Knjige</h3>
 
-<table class="table table-striped">
-    <tr>
-        <th>Naslov</th>
-        <th>Autor</th>
-        <th>Kategorija</th>
-        <th>Akcije</th>
-    </tr>
+<a href="index.php?action=create" class="btn btn-success mb-2">Dodaj knjigu</a>
 
-    <?php foreach($books as $b): ?>
-    <tr>
-        <td><?= $b['title'] ?></td>
-        <td><?= $b['author'] ?></td>
-        <td><?= $b['category'] ?></td>
-        <td>
-            <a href="index.php?action=edit&id=<?= $b['id'] ?>" class="btn btn-warning btn-sm">Edit</a>
-            <a href="index.php?action=delete&id=<?= $b['id'] ?>" 
-               class="btn btn-danger btn-sm delete-btn">Delete</a>
-        </td>
-    </tr>
-    <?php endforeach; ?>
+<input type="text" id="search" class="form-control mb-2" placeholder="Pretraga...">
+
+<table class="table table-bordered table-hover">
+<thead>
+<tr>
+    <th>Naslov</th>
+    <th>Autor</th>
+    <th>Kategorija</th>
+    <th>Akcije</th>
+</tr>
+</thead>
+
+<tbody>
+<?php foreach($books as $b): ?>
+<tr>
+    <td><?= $b['title'] ?></td>
+    <td><?= $b['author'] ?></td>
+    <td><?= $b['category'] ?></td>
+    <td>
+        <a href="index.php?action=edit&id=<?= $b['id'] ?>" class="btn btn-warning btn-sm">Edit</a>
+        <a href="index.php?action=delete&id=<?= $b['id'] ?>" class="btn btn-danger btn-sm delete-btn">Delete</a>
+    </td>
+</tr>
+<?php endforeach; ?>
+</tbody>
 </table>
 
 <?php include "../views/layout/footer.php"; ?>
