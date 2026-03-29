480fd7bc8d5e27ae468cadc0f40746a98bbf36e4
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-29 13:03:36 +0200

    Update script.js

--- mvc-projekat/public/script.js
+++ mvc-projekat/public/script.js
@@ -1,1 +1,8 @@
 
+document.querySelectorAll('.delete-btn').forEach(btn => {
+    btn.addEventListener('click', function(e) {
+        if (!confirm("Da li ste sigurni?")) {
+            e.preventDefault();
+        }
+    });
+});
