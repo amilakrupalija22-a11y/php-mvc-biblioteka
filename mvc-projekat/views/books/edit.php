dcecb1e02f34e4f0aaff21f60b23468a7896de40
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-29 13:12:16 +0200

    Update edit.php

--- mvc-projekat/views/books/edit.php
+++ mvc-projekat/views/books/edit.php
@@ -1,13 +1,23 @@
 <?php include "../views/layout/header.php"; ?>
 
+<h3>Edit knjige</h3>
+
 <form method="POST">
     <input type="hidden" name="id" value="<?= $book['id'] ?>">
 
-    <input type="text" name="title" value="<?= $book['title'] ?>" class="form-control mb-2">
-    <input type="text" name="author" value="<?= $book['author'] ?>" class="form-control mb-2">
-    <input type="number" name="category_id" value="<?= $book['category_id'] ?>" class="form-control mb-2">
+    <input name="title" value="<?= $book['title'] ?>" class="form-control mb-2">
+    <input name="author" value="<?= $book['author'] ?>" class="form-control mb-2">
 
-    <button class="btn btn-success">Update</button>
+    <select name="category_id" class="form-control mb-2">
+        <?php foreach($categories as $c): ?>
+            <option value="<?= $c['id'] ?>"
+                <?= $c['id'] == $book['category_id'] ? 'selected' : '' ?>>
+                <?= $c['name'] ?>
+            </option>
+        <?php endforeach; ?>
+    </select>
+
+    <button class="btn btn-primary">Update</button>
 </form>
 
 <?php include "../views/layout/footer.php"; ?>
