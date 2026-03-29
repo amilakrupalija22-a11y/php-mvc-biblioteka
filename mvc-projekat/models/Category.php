0a5e263ed62364438865cd0e03737c449620c886
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-29 13:04:10 +0200

    Update Category.php

--- mvc-projekat/models/Category.php
+++ mvc-projekat/models/Category.php
@@ -1,1 +1,12 @@
+<?php
+class Category {
+    private $conn;
 
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    public function getAll() {
+        return $this->conn->query("SELECT * FROM categories");
+    }
+}
