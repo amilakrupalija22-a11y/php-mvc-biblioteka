f40fa28fba52a11c19c90c6a39c86425ce843bef
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-29 13:02:22 +0200

    Refactor book creation form layout and inputs

--- mvc-projekat/views/books/create.php
+++ mvc-projekat/views/books/create.php
@@ -1,15 +1,11 @@
-<form method="POST" action="index.php?action=store" class="container mt-5">
+<?php include "../views/layout/header.php"; ?>
 
-    <input type="text" name="title" placeholder="Naslov" class="form-control mb-2" required>
-    <input type="text" name="author" placeholder="Autor" class="form-control mb-2" required>
+<form method="POST">
+    <input type="text" name="title" class="form-control mb-2" placeholder="Naslov" required>
+    <input type="text" name="author" class="form-control mb-2" placeholder="Autor" required>
+    <input type="number" name="category_id" class="form-control mb-2" placeholder="Category ID">
 
-    <select name="category_id" class="form-control mb-2" required>
-        <option value="">Odaberi kategoriju</option>
-        <?php foreach($categories as $cat): ?>
-            <option value="<?= $cat['id'] ?>"><?= $cat['name'] ?></option>
-        <?php endforeach; ?>
-    </select>
-
-    <button class="btn btn-primary">Sačuvaj</button>
-
+    <button class="btn btn-success">Spasi</button>
 </form>
+
+<?php include "../views/layout/footer.php"; ?>
