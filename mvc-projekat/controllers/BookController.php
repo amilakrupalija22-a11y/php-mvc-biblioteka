f7f4b748d4d3314dc65ce7e500b11e0ee2f004eb
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-29 13:09:45 +0200

    Update BookController.php

--- mvc-projekat/controllers/BookController.php
+++ mvc-projekat/controllers/BookController.php
@@ -1,11 +1,14 @@
 <?php
 require_once "../models/Book.php";
+require_once "../models/Category.php";
 
 class BookController {
     private $book;
+    private $category;
 
     public function __construct($db) {
         $this->book = new Book($db);
+        $this->category = new Category($db);
     }
 
     public function index() {
@@ -14,24 +17,39 @@ class BookController {
     }
 
     public function create() {
+        $categories = $this->category->getAll();
+
         if ($_POST) {
-            $this->book->create($_POST['title'], $_POST['author'], $_POST['category_id']);
+            $this->book->create(
+                $_POST['title'],
+                $_POST['author'],
+                $_POST['category_id']
+            );
             header("Location: index.php");
         }
+
         include "../views/books/create.php";
+    }
+
+    public function edit() {
+        $categories = $this->category->getAll();
+
+        if ($_POST) {
+            $this->book->update(
+                $_POST['id'],
+                $_POST['title'],
+                $_POST['author'],
+                $_POST['category_id']
+            );
+            header("Location: index.php");
+        }
+
+        $book = $this->book->getById($_GET['id']);
+        include "../views/books/edit.php";
     }
 
     public function delete() {
         $this->book->delete($_GET['id']);
         header("Location: index.php");
-    }
-
-    public function edit() {
-        if ($_POST) {
-            $this->book->update($_POST['id'], $_POST['title'], $_POST['author'], $_POST['category_id']);
-            header("Location: index.php");
-        }
-        $book = $this->book->getById($_GET['id']);
-        include "../views/books/edit.php";
     }
 }
