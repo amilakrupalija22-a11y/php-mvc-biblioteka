848833ec1609608f6a1ce250baa6c52d09324ca9
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-29 13:00:53 +0200

    Add HTML structure to header.php

--- mvc-projekat/views/layout/header.php
+++ mvc-projekat/views/layout/header.php
@@ -1,1 +1,10 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Biblioteka</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container mt-4">
+<h2 class="text-center">📚 Biblioteka knjiga</h2>
+<hr>
