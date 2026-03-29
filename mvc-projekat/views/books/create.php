67d745e95cc508d7ba4be6a8ea4362c89963b38f
Amila Krupalija <amilakrupalija22@gmail.com>
2026-03-29 13:11:53 +0200

    Update book creation form with category selection

--- mvc-projekat/views/books/create.php
+++ mvc-projekat/views/books/create.php
@@ -1,9 +1,16 @@
 <?php include "../views/layout/header.php"; ?>
 
+<h3>Dodaj knjigu</h3>
+
 <form method="POST">
-    <input type="text" name="title" class="form-control mb-2" placeholder="Naslov" required>
-    <input type="text" name="author" class="form-control mb-2" placeholder="Autor" required>
-    <input type="number" name="category_id" class="form-control mb-2" placeholder="Category ID">
+    <input name="title" class="form-control mb-2" placeholder="Naslov" required>
+    <input name="author" class="form-control mb-2" placeholder="Autor" required>
+
+    <select name="category_id" class="form-control mb-2">
+        <?php foreach($categories as $c): ?>
+            <option value="<?= $c['id'] ?>"><?= $c['name'] ?></option>
+        <?php endforeach; ?>
+    </select>
 
     <button class="btn btn-success">Spasi</button>
 </form>
